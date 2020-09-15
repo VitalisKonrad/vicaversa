f8541415dfa68c5cff87f6b142fc254eb7439a47
VitalisKonrad <46555488+VitalisKonrad@users.noreply.github.com>
2020-09-15 17:59:09 +0300

    fix whitespace

--- vicaversa-project/vicaversa/views.py
+++ vicaversa-project/vicaversa/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    return render(request, 'reverse.html', {'key_usertext' : user_text})
